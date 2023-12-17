f5086affaf0430962e4cebfe5b6be21fb309ba8a
michael-lemelin <mikelemelin1515@gmail.com>
2023-12-16 19:38:00 -0500

    Removed starting HelloWorld component and added about me and projects components to start.

---
+++ src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+    
+</template>
+
+<script>
+import AboutMe from './AboutMe.vue'
+import Projects from './Projects.vue'
+
+export default({
+    components() {
+        AboutMe,
+        Projects
+    },
+})
+</script>
